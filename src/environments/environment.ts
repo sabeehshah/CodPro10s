e075bd6c6b79bc377fbeea7442572e73719ef2cb
Sabeeh Shah <sabeeh.shah@hotmail.com>
2018-10-25 18:53:28 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyBtWFuAt8A7tILGafsEUOq6_FRFZ3H3j3Y",
-    authDomain: "codpro10-512e0.firebaseapp.com",
-    databaseURL: "https://codpro10-512e0.firebaseio.com",
-    projectId: "codpro10-512e0",
-    storageBucket: "codpro10-512e0.appspot.com",
-    messagingSenderId: "836732248755"
+   firebase: {
+    apiKey: "<YOUR_OWN>",
+    authDomain: "<YOUR_OWN>",
+    databaseURL: "<YOUR_OWN>",
+    projectId: "<YOUR_OWN>",
+    storageBucket: "<YOUR_OWN>",
+    messagingSenderId: "<YOUR_OWN>"
   }
 };
 
